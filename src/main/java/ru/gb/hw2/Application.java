581981b2d0d0652fc298b579e7be0cc5eabd3f99
«Andrey_Istomin» <an.istomin@gmail.com>
2024-09-21 00:37:06 +0500

    Add ReadMe file

--- src/main/java/ru/gb/hw2/Application.java
+++ src/main/java/ru/gb/hw2/Application.java
@@ -1,9 +1,5 @@
 package ru.gb.hw2;
 
-/** Используя Spring Boot, создайте простое веб-приложение, которое приветствует
- * пользователя на главной странице.
- */
-
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
